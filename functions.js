aea0dfb4af69c310c1db3e060d34345ec5292ba3
spencerhallam <spencerhallam@gmail.com>
2019-10-24 21:36:07 -0500

    Finished Code
    Added style sheet, refined comments, syntax, and spacing prior to review.

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+
+// Data for customer transactions in 2019. "amt" = "amount spent" in each purchase
 const transactions = [
     {
         id: 10001,
@@ -8,13 +10,13 @@ const transactions = [
     {
         id: 10001,
         customer: "Luna",
-        date: "2019-10-02",
-        amt: 5000,  
+        date: "2019-9-02",
+        amt: 232,  
     },
     {
         id: 10001,
         customer: "Luna",
-        date: "2019-9-06",
+        date: "2019-10-06",
         amt: 146,  
     },
     {
@@ -58,59 +60,61 @@ const transactions = [
         customer: "Helen",
         date: "2019-2-20",
         amt: 110,  
-    }
+    },
+    {
+        id: 10005,
+        customer: "Nina",
+        date: "2019-8-20",
+        amt: 165,  
+    },
+    {
+        id: 10002,
+        customer: "Francis",
+        date: "2019-5-28",
+        amt: 148,  
+    },
+    {
+        id: 10003,
+        customer: "Helen",
+        date: "2019-7-13",
+        amt: 30,  
+    },
 ]
 
-//3 months to search use number "0-11"
-const monthsToFilter = [ 7, 8, 9 ];
+const monthsToFilter = [ 7, 8, 9 ]; // 3 months to search use number "0-11"
 
-console.log("Original Data:", JSON.stringify(transactions));
+// Variables
+const subTotalArray = []; // stores points calculated for each transaction
+const custForEachTrans = []; // stores a customer id for each transaction
+let unique = []; //  stores customer ids with duplicates removed
 
-//variables
-const subTotalArray = []; //stores points calculated for each transaction
-const custForEachTrans = []; //stores a customer id for each transaction
-let unique = []; // stores customer ids with duplicates removed
-
-
-//Main Function Stack
+// Main Function Stack
 function getCustPointsReport(){
-    //Calculate Points for each transaction & generate array of customer ids for each transaction
-    let sums = calcPoints(transactions);
-    console.log("subTotalArray:", subTotalArray);
-    console.log("custForEachTrans:", custForEachTrans);
+    // Calculate Points for each transaction & generate array of customer ids for each transaction
+    calcPoints(transactions);
 
-    //Filter Month Range
+    // Filter Month Range
     let monthFilter = chooseMonths(subTotalArray, monthsToFilter);
-    console.log("monthFilter:", monthFilter);
 
-    //get list of customer ids and return array with no duplicates
+    // get list of customer ids and return array with no duplicates
     let custIds = removeDupes(custForEachTrans);
-    console.log("custIds:", custIds);
    
-    //Get 3 month point totals for each customer
+    // Get 3 month point totals for each customer
     let threeMoSums = getCustSums(custIds, monthFilter);
-    console.log("threeMoSums: ", threeMoSums);
    
-    //Get single month totals for month for each customer
+    // Get single month totals for each month/customer
     let singleMoTots = getCustMoSums(monthFilter, custIds, monthsToFilter)
-    console.log("singleMoTots:", singleMoTots);
-    
-    // let singleMoTotsDry = removeObjDups(singleMoTots);
-    // console.log("singleMoTotsDry:", singleMoTotsDry);
-
-    console.log("UNIUNI:", unique);
-    console.log("sums:", sums);
 
     sumDisplay(threeMoSums);
-    moSumDisplay(singleMoTots)
+    moSumDisplay(singleMoTots);
+
 }
 
-//MAIN FUNCTIONS
-
+// Main functions
 function calcPoints(data) {
     let sums;
 
-    //Calculate reward points
+    // Calculate reward points
     for(i in data){
         let totOne = 0;
         if(data[i].amt > 50 && data[i].amt < 100){
@@ -120,21 +124,23 @@ function calcPoints(data) {
             totOne += 50 + (data[i].amt - 100)*2
         }
 
-    //Create object containing customerId to calculated points for each transaction.
+    // Create object containing customerId to calculated points for each transaction.
     let dateObj = new Date(data[i].date);
     let monthCode = dateObj.getMonth();
     let subpoints = {customerId: data[i].id, name: data[i].customer,  date: data[i].date, month: monthCode, points: totOne };
    
-    //Push customer ids into their own array
+    // Push customer ids into their own array
     custForEachTrans.push(data[i].id);
 
-    //Push object with customerId and earned transaction points to array
+    // Push object with customerId and earned transaction points to array
     subTotalArray.push(subpoints);
    }
 
 }
 
+// Use array of month numbers to filter out unwanted data
 function chooseMonths(arr, months){
+
      let newArr = [];
 
      for(i in arr){
@@ -144,11 +150,13 @@ function chooseMonths(arr, months){
      }
 
      return newArr
+
 }
 
 
-//Uses unique customer ids to search through and total earned points for each customer filtered by months indicated
+// Uses unique customer ids to search through and total earned points for each customer filtered by months indicated
 function getCustSums(arrA, arrB){
+
   let superSum = [];  
   for(i in arrA){
 
@@ -163,33 +171,36 @@ function getCustSums(arrA, arrB){
        
             pointTotA += arrB[j].points
             nameA = arrB[j].name
-            //dateA = arrB[j].month
             transQty += 1
-        }
-      }  
 
-       //Create new object and push to "superSum" array
-      let custSum = {id: arrA[i], name: nameA, total: pointTotA, qty: transQty};
-         superSum.push(custSum);
+        }
+    }  
+
+        // Create new object and push to "superSum" array
+        let custSum = {id: arrA[i], name: nameA, total: pointTotA, qty: transQty};
+        superSum.push(custSum);
     }
 
   return superSum
+
 }
 
+// Uses unique user and month filter array to total points for each customer/month
 function getCustMoSums(arrA, arrB, months){
-    let superSum = []; 
+
+    let superSum = [];
 
     for(i in arrB){
         for(j in months){
-            
+
             let pointTotA = 0;
-            let nameA = "See ID";
+            let nameA = "<em>see id</em>";
             let monthsA = months[j];
             let transQty = 0;
-            let idA = 0; 
+            let idA = 0;
 
                 for(k in arrA){
-                    
+                   
                     if(arrA[k].customerId === arrB[i] && arrA[k].month === months[j] && arrA[k].customerId !== 0 ){
 
                         monthA = months[j]
@@ -197,31 +208,26 @@ function getCustMoSums(arrA, arrB, months){
                         idA = arrA[k].customerId
                         transQty += 1
                         pointTotA += arrA[k].points
-                        
+                       
                     }
+                }
 
-         }
-
-
-     let custSum = {id: arrB[i], name: nameA, month: monthsA, total: pointTotA, qty: transQty};
-     if(custSum.id !== 0){
-     superSum.push(custSum); 
-     }
+                // Create new object and push to "superSum" array
+                let custSum = {id: arrB[i], name: nameA, month: monthsA, total: pointTotA, qty: transQty};
+                if(custSum.id !== 0){
+                superSum.push(custSum);
+            }
+        }
     }
 
-    
-    
-  }
- 
-  return superSum
+    return superSum
+
 }
 
-
-
-//Removes duplicates values from any array
+// Removes duplicates values from any array
 function removeDupes(arrWithDupes){
     let uniq = [...new Set(arrWithDupes)];
-    //console.log("uniq", uniq);
+    // console.log("uniq", uniq);
     return uniq;
 }
 
@@ -234,54 +240,36 @@ function getTots(arr){
   return superSum
 }
 
-//Removes duplicates values from any array
+// Removes duplicates values from any array
 function removeDupes(arrWithDupes){
     let uniq = [...new Set(arrWithDupes)];
-    //console.log("uniq", uniq);
+    // console.log("uniq", uniq);
     return uniq;
 }
 
-
-//Removes duplicate objects from any array
-function removeObjDups(x) {
-
-            // Create an array of objects  
-            jsonObject = x.map(JSON.stringify);
-     
-            //console.log(jsonObject);  
-            uniqueSet = new Set(jsonObject);
-            uniqueArray = Array.from(uniqueSet).map(JSON.parse);
-     
-            //console.log(uniqueArray);
-            return uniqueArray
-        }
-
-
-
-
-
-//Outputs Totals to the DOM
+// Renders customer point totals to the DOM
 function sumDisplay(arrayX){
      
     for(i in arrayX){
-        const paraA = document.createElement("P");
-        paraA.innerHTML = arrayX[i].name + " | " + arrayX[i].id + " | 3 Month Total: " + arrayX[i].total + " points";
-        document.getElementById("main").appendChild(paraA);
+        const paraA = document.createElement("LI");
+        paraA.innerHTML = "<strong>ID#</strong> " + arrayX[i].id + " | " +  arrayX[i].name + " | 3 month total: <strong>" + arrayX[i].total + "</strong> points";
+        document.getElementById("summary").appendChild(paraA);
     }
 
 }
+
+// Renders customer monthly point totals to the DOM
 function moSumDisplay(arrayX){      
     console.log('arrayX', arrayX);
     for(i in arrayX){
-        const paraB = document.createElement("P");
-        //console.log("paraA", paraA);
-        paraB.innerHTML = arrayX[i].name + " | " + arrayX[i].id + " | Total/Mo: " + arrayX[i].total + " points" + " | " + "Transaction qty: " + arrayX[i].qty;
-        document.getElementById("main").appendChild(paraB);
+        const paraB = document.createElement("LI");
+        paraB.innerHTML = "<strong>ID#</strong> " + arrayX[i].id + " | " +  arrayX[i].name + " | month: " + getMonthString(arrayX[i].month) +" | total/Mo: <strong>" + arrayX[i].total + "</strong> points" + " | " + "qty: " + arrayX[i].qty;
+        document.getElementById("month-tots").appendChild(paraB);
     }
 
 }
 
-//HELPER FUNCTIONS:
+// Utility function:
 function getMonthString(num){
     let month;
     switch(num)
